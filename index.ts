c798248a0c90ee2181b47476d58e0c2f08a9088c
Muhammad Umar <164160512+Muhammadumar4296@users.noreply.github.com>
2024-04-12 05:24:37 -0700

    Todos-List

---
+++ index.ts
@@ -0,0 +1,27 @@
+import inquirer from "inquirer"
+ let todos = [];
+ let condition = true;
+
+ while(condition)
+ {
+let addTask = await inquirer.prompt(
+   [
+    {
+      name: 'todo',
+      type: 'input',
+      message: 'Eid ki kya kya shopping ki hai'
+    },
+
+    {
+      name: 'addMore',
+      type: 'confirm',
+      message: 'aur kya kya kharaeda',
+      default: 'false'
+
+    }
+   ]
+)
+todos.push(addTask.todo);
+condition =addTask.addMore
+console.log(todos)
+ }
